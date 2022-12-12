b0b89653b3d020fdb19dca091ec26f3a7d8399e0
gabrielgusso <gabriel.gusso@hotmail.com>
2022-12-12 14:33:29 -0300

    Feat: rentals DELETE route

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -33,17 +33,19 @@ export async function rentalsGetController(req, res) {
   const custumerId = req.query.custumerId
   const gameId = req.query.gameId
 
-  if(custumerId){
+  if (custumerId) {
     const custumerRental = await connection.query(
-      `SELECT rentals.*, JSON_BUILD_OBJECT('name', customers.name, 'id', customers.id) AS customers, JSON_BUILD_OBJECT('name', games.name, 'id', games.id, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game FROM customers JOIN rentals ON customers.id = rentals."customerId" JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id WHERE "customerId"=$1`, [custumerId]
+      `SELECT rentals.*, JSON_BUILD_OBJECT('name', customers.name, 'id', customers.id) AS customers, JSON_BUILD_OBJECT('name', games.name, 'id', games.id, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game FROM customers JOIN rentals ON customers.id = rentals."customerId" JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id WHERE "customerId"=$1`,
+      [custumerId]
     )
     res.send(custumerRental.rows)
     return
   }
 
-  if(gameId){
+  if (gameId) {
     const gameRental = await connection.query(
-      `SELECT rentals.*, JSON_BUILD_OBJECT('name', customers.name, 'id', customers.id) AS customers, JSON_BUILD_OBJECT('name', games.name, 'id', games.id, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game FROM customers JOIN rentals ON customers.id = rentals."customerId" JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id WHERE "gameId"=$1`, [gameId]
+      `SELECT rentals.*, JSON_BUILD_OBJECT('name', customers.name, 'id', customers.id) AS customers, JSON_BUILD_OBJECT('name', games.name, 'id', games.id, 'categoryId', games."categoryId", 'categoryName', categories.name) AS game FROM customers JOIN rentals ON customers.id = rentals."customerId" JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id WHERE "gameId"=$1`,
+      [gameId]
     )
     res.send(gameRental.rows)
     return
@@ -59,24 +61,28 @@ export async function rentalsResturnPostController(req, res) {
   const id = req.params.id
   let today = dayjs().locale("pt-br").format("YYYY-MM-DD")
   let dalayFee = 0
-  const searchRentDay = await connection.query('SELECT rentals."rentDate", rentals."daysRented", rentals."originalPrice", rentals."returnDate" FROM rentals WHERE id=$1', [id])
+  const searchRentDay = await connection.query(
+    'SELECT rentals."rentDate", rentals."daysRented", rentals."originalPrice", rentals."returnDate" FROM rentals WHERE id=$1',
+    [id]
+  )
 
-  if(!id){
+  if (!id) {
     res.sendStatus(404)
     return
   }
-  if(searchRentDay.rows[0].returnDate !== null){
+  if (searchRentDay.rows[0].returnDate !== null) {
     res.sendStatus(400)
     return
   }
   const rendDay = searchRentDay.rows[0].rentDate
   let daysRentdAtNow = dayjs(today).diff(rendDay, "day")
   const tax = daysRentdAtNow - searchRentDay.rows[0].daysRented
-  const pricePerDay = searchRentDay.rows[0].originalPrice / searchRentDay.rows[0].daysRented 
-  if(tax > 0){
+  const pricePerDay =
+    searchRentDay.rows[0].originalPrice / searchRentDay.rows[0].daysRented
+  if (tax > 0) {
     dalayFee = tax * pricePerDay
-  } 
-  
+  }
+
   try {
     await connection.query(
       'UPDATE rentals SET "returnDate"=$1, "delayFee"=$2 WHERE id=$3',
@@ -85,5 +91,29 @@ export async function rentalsResturnPostController(req, res) {
     res.sendStatus(201)
   } catch (error) {
     console.log(error)
+  }
+}
+
+export async function rentalsDeleteController(req, res) {
+  const id = req.params.id
+
+  const rentals = await connection.query(`SELECT * FROM rentals WHERE id=$1`, [
+    id,
+  ])
+  if (!rentals.rows[0]) {
+    res.sendStatus(404)
+    return
+  }
+  if (rentals.rows[0].returnDate === null) {
+    res.sendStatus(400)
+    return
+  }
+
+  try {
+    await connection.query(`DELETE FROM rentals WHERE id=$1;`, [id])
+    res.sendStatus(200)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
   }
 }
--- src/routes/rentals.router.js
+++ src/routes/rentals.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { rentalsMiddleware } from "../middlewares/rentals.middleware.js"
-import { rentalsPostController, rentalsGetController, rentalsResturnPostController } from "../controllers/rentals.controller.js"
+import { rentalsPostController, rentalsGetController, rentalsResturnPostController, rentalsDeleteController } from "../controllers/rentals.controller.js"
 
 export const rentalsRoute = Router()
 
@@ -9,3 +9,6 @@ rentalsRoute.get("/rentals", rentalsGetController)
 rentalsRoute.post("/rentals", rentalsMiddleware, rentalsPostController)
 
 rentalsRoute.post("/rentals/:id/return", rentalsResturnPostController)
+
+
+rentalsRoute.delete("/rentals/:id", rentalsDeleteController)
